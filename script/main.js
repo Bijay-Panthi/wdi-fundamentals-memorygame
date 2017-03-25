4d08f6a20abe85e2d24675ddad7ca2d97657a13c
Bijay Panthi <bijaypanthi@gmail.com>
2017-03-26 08:33:56 +1100

    created object within array and added file location to display within console

--- script/main.js
+++ script/main.js
@@ -1,6 +1,29 @@
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank : "queen",
+		suit : "hearts",
+		cardImage : "images/queen-of-hearts.png"
+	},
+
+	{
+		rank : "queen",
+		suit : "diamonds",
+		cardImage : "images/queen-of-diamonds.png"
+	},
+	{
+		rank : "king",
+		suit : "hearts",
+		cardImage : "images/king-of-hearts.png"
+	},
+	{
+		rank : "king",
+		suit : "diamonds",
+		cardImage : "images/king-of-hearts.png"
+	}
+
+];
 
 var cardsInPlay = [];
 
@@ -19,16 +42,19 @@ function checkForMatch()
 }
 
 
-function flipcard(cardId)
+function flipCard(cardId)
 {
 
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	//????????????
+	cardsInPlay.push(cards[cardId].rank);
 
-	cardsInPlay.push(cards[cardId]);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
 	checkForMatch();
 }
 
-flipcard(0);
-flipcard(2);
+flipCard(0);
+flipCard(2);
 
